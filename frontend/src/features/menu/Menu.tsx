7ab0ee2251d777ad6c639ab230be74c555af4268
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-03 18:02:31 -0400

    abstracted menu into its own component

--- frontend/src/features/menu/Menu.tsx
+++ frontend/src/features/menu/Menu.tsx
@@ -1,11 +1,24 @@
 import styles from './Menu.module.css'
-import { Layout } from 'antd'
+import { ArrowsFullscreen, Boxes, CalendarWeek, Question } from 'react-bootstrap-icons'
 
 
 const Menu = () => {
   return (
-    <div className={styles.menu}>
-      header
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <button className={styles.button}>
+          <Boxes className={styles.icon}/>
+        </button>
+        <button className={styles.button}>
+          <CalendarWeek className={styles.icon}/>
+        </button>
+        <button className={styles.button}>
+          <Question className={styles.icon}/>
+        </button>
+        <button className={styles.button}>
+          <ArrowsFullscreen className={styles.icon}/>
+        </button>
+      </div>
     </div>
   )
 }
--- frontend/src/features/page/Page.tsx
+++ frontend/src/features/page/Page.tsx
@@ -1,17 +1,53 @@
 import styles from './Page.module.css'
-import { Layout, Space } from 'antd'
-import Header from '../header/Header'
-import Content from '../content/Content'
+import { Layout, Space, Input } from 'antd' 
+import Content from '../contentd/Content'
+import profilePic from '../../profile.jpeg'
+
+import React from 'react';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown } from 'antd';
+import { ArrowsFullscreen, Boxes, Calendar2, CalendarWeek, Fullscreen, Question } from 'react-bootstrap-icons';
+import Menu from '../menu/Menu'
+
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: (
+      <>
+        {'Profile'}
+      </>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <>
+        {'Settings'}
+      </>
+    )
+  }
+]
+
 
 
 const Page = () => {
   return (
-    <Space direction="vertical" className={styles.space}>
-      <Layout className={styles.layout}>
-        <Header/>
+    <section className={styles.mainSection}>
+      <Menu/>
+      <div className={styles.contentWrapper}>
+        <header className={styles.header}>
+          <Input.Search className={styles.search} placeholder="search" style={{ width: 200 }} />
+          <Dropdown menu={{ items }}>
+            <Space>
+              <img src={profilePic} className={styles.profileImg}/>
+              <DownOutlined />
+            </Space>
+          </Dropdown>
+        </header>
         <Content/>
-      </Layout>
-    </Space>
+      </div>
+    </section>
   )
 }
 

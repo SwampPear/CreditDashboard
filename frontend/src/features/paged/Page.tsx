78fda93a296f942ae3e1f735a7e08ef669e40131
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-03 17:58:19 -0400

    implemented side menu

--- frontend/src/features/paged/Page.tsx
+++ frontend/src/features/paged/Page.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Dropdown } from 'antd';
+import { ArrowsFullscreen, Boxes, Calendar2, CalendarWeek, Fullscreen, Question } from 'react-bootstrap-icons';
 
 const items: MenuProps['items'] = [
   {
@@ -33,7 +34,20 @@ const Page = () => {
   return (
     <section className={styles.mainSection}>
       <div className={styles.menuWrapper}>
-        sadf
+        <div className={styles.menu}>
+          <button className={styles.menuButton}>
+            <Boxes className={styles.menuButtonIcon}/>
+          </button>
+          <button className={styles.menuButton}>
+            <CalendarWeek className={styles.menuButtonIcon}/>
+          </button>
+          <button className={styles.menuButton}>
+            <Question className={styles.menuButtonIcon}/>
+          </button>
+          <button className={styles.menuButton}>
+            <ArrowsFullscreen className={styles.menuButtonIcon}/>
+          </button>
+        </div>
       </div>
       <div className={styles.contentWrapper}>
         <header className={styles.header}>

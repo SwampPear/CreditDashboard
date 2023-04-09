987982e8510330dd346e176e0514f2c60bb883e5
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-09 00:54:14 -0400

    adjusting styling to accommodate for responsiveness

--- frontend/src/features/charts/Charts.tsx
+++ frontend/src/features/charts/Charts.tsx
@@ -11,28 +11,23 @@ interface IChartsProps {
 }
 
 const Charts = (props: IChartsProps) => {
-  const [totalProductCount, setTotalProductCount] = useState<number>(0)
-  const [totalMemberCount, setTotalMemberCount] = useState<number>(0)
-
   return (
     <>
       <div className={styles.chartContainer}>
-        <Row gutter={[32, 32]}>
+        <Row gutter={[24, 24]}>
           <Col className={styles.col}>
-
             <div className={styles.widgetContainer}>
-              <Widget header="Total Product Count" content={totalProductCount}/>
-              <Widget header="Total Member Count" content={totalMemberCount}/>
-              <Widget header="asdf" content={69}/>
-              <Widget header="asdf" content={69}/>
+              <Widget header="d" content={'asdf'}/>
+              <Widget header="d" content={'asdf'}/>
+              <Widget header="d" content={'asdf'}/>
+              <Widget header="d" content={'asdf'}/>
             </div>
-            
           </Col>
           <Col className={styles.col}>
             <Chart type='Pie' data={props.data}/>
           </Col>
         </Row>
-        <Row gutter={[32, 32]}>
+        <Row gutter={[24, 24]}>
           <Col className={styles.col}>
             <Chart type='RadialBar' data={props.data}/>
           </Col>

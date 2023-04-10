8ed07542e0fe3d357457be0a40c6750addac6f08
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-10 00:01:26 -0400

    set all widgets with useful information

--- frontend/src/features/charts/Charts.tsx
+++ frontend/src/features/charts/Charts.tsx
@@ -11,16 +11,70 @@ interface IChartsProps {
 }
 
 const Charts = (props: IChartsProps) => {
+  const [totalProducts, setTotalProducts] = useState<number>(0)
+  const [totalMembers, setTotalMembers] = useState<number>(0)
+  const [maxLifeStage, setMaxLifeStage] = useState<string>('')
+  const [preferredChannel, setPreferredChannel] = useState<string>('')
+
+  useEffect(() => {
+    if (props.data) {
+      for (let i = 0; i < props.data?.length; i++) {
+        const dataSet = props.data?.[i].Name
+        let max = 0
+        let idx
+
+        switch (dataSet) {
+          case 'CurrentProducts':
+            setTotalProducts(props.data?.[i].XData.length)
+            break
+          
+          case 'Levels':
+            setTotalMembers(props.data?.[i].YData.reduce((sum, a) => sum + a, 0))
+            break
+
+          case 'LifeStage':
+            max = 0
+            idx = 0
+
+            for (let j = 0; j < props.data?.[i].YData.length; j++) {
+              if (props.data?.[i].YData[j] > max) {
+                max = props.data?.[i].YData[j]
+                idx = j
+              }
+            }
+
+            setMaxLifeStage(props.data?.[i].XData[idx])
+            break
+
+          case 'PreferredChannel':
+            max = 0
+            idx = 0
+
+            for (let j = 0; j < props.data?.[i].YData.length; j++) {
+              if (props.data?.[i].YData[j] > max) {
+                max = props.data?.[i].YData[j]
+                idx = j
+              }
+            }
+
+            setPreferredChannel(props.data?.[i].XData[idx])
+            break
+        }
+      }
+    }
+    
+  }, [props.data])
+
   return (
     <>
       <div className={styles.chartContainer}>
         <Row className={styles.row} gutter={[24, 24]}>
           <Col className={styles.col}>
             <div className={styles.widgetContainer}>
-              <Widget header="d" content={'asdf'}/>
-              <Widget header="d" content={'asdf'}/>
-              <Widget header="d" content={'asdf'}/>
-              <Widget header="d" content={'asdf'}/>
+              <Widget header="Total Products" content={totalProducts}/>
+              <Widget header="Total Members" content={totalMembers}/>
+              <Widget header="Max Life Stage" content={maxLifeStage}/>
+              <Widget header="Preferred Channel" content={preferredChannel}/>
             </div>
           </Col>
           <Col className={styles.col}>

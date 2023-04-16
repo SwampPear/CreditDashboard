6ce61e310dba479af6b99f1591382ad32003987b
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-15 21:25:39 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Credit Dashboard
+## Overiew
--- frontend/src/features/colorThemeSettings/ColorThemeSettings.tsx
+++ frontend/src/features/colorThemeSettings/ColorThemeSettings.tsx
@@ -1,11 +1,9 @@
-import styles from './ColorThemeSettings.module.css'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { selectColorTheme, toggle, ColorTheme } from './ColorThemeSettingsSlice'
+import { useAppDispatch } from '../../app/hooks'
+import { toggle, ColorTheme } from './ColorThemeSettingsSlice'
 import { Radio } from 'antd'
 
 const ColorThemeSettings = () => {
   const dispatch = useAppDispatch()
-  const colorTheme = useAppSelector(selectColorTheme)
 
   return (
     <Radio.Group defaultValue="light">
